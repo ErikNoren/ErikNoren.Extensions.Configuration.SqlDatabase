a4f9bdf3fb40da78ebd650ec3a24b995a68b5456
mkbmain <michael.bourke@live.co.uk>
2022-09-10 05:44:18 +0100

    Refactor tests

--- tests/SqlServerReadTests.cs
+++ tests/SqlServerReadTests.cs
@@ -1,58 +1,53 @@
 using System.Data.Common;
 using Microsoft.Data.SqlClient;
 using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 
 namespace ErikNoren.Extensions.Configuration.SqlServer.Tests;
 
 public class SqlServerReadTests
 {
-    string SqlServerConnectionString => @"Server=localhost;Database=Config;User Id=sa;Password=A1234567a;";
-    string MySqlServerConnectionString => @"Server=localhost;Database=Config;Uid=root;Pwd=A1234567a;";
+    const string SqlServerConnectionString = @"Server=localhost;Database=Config;User Id=sa;Password=A1234567a;";
+    const string MySqlServerConnectionString = @"Server=localhost;Database=Config;Uid=root;Pwd=A1234567a;";
+    const string SqlQuery = @"SELECT SettingKey, SettingValue FROM Settings WHERE IsActive = 1";
 
     [Fact]
     public void DatabaseSettingsAreLoadable_SqlServer()
     {
-        using var cfgProvider =
-            new SqlServerConfigurationProvider<SqlConnection>(new SqlServerConfigurationSource<SqlConnection>()
-            {
-                DbConnection = () => new SqlConnection(SqlServerConnectionString),
-                CreateQueryDelegate = db =>
-                    new SqlCommand("SELECT SettingKey, SettingValue FROM dbo.Settings WHERE IsActive = 1", db)
-            });
+        using var cfgProvider = BuildConfig<SqlConnection, SqlCommand>(
+            () => new SqlConnection(SqlServerConnectionString), db =>
+                new SqlCommand(SqlQuery, db));
 
         cfgProvider.Load();
-
-        Assert.True(cfgProvider.TryGet("AppSettings:DbIntSetting", out var value));
-        Assert.Equal("769283", value);
-
-        Assert.False(cfgProvider.TryGet("AppSettings:InactiveSetting", out _));
+        AssertConfig(cfgProvider);
     }
 
 
     [Fact]
     public void DatabaseSettingsAreLoadable_MySql()
     {
-        using var cfgProvider =
-            new SqlServerConfigurationProvider<MySqlConnection>(new SqlServerConfigurationSource<MySqlConnection>()
-            {
-                DbConnection = () => new MySqlConnection(MySqlServerConnectionString),
-                CreateQueryDelegate = db =>
-                    new MySqlCommand("SELECT SettingKey, SettingValue FROM Settings WHERE IsActive = 1", db)
-            });
-
-        cfgProvider.Load();
-
-        Assert.True(cfgProvider.TryGet("AppSettings:DbIntSetting", out var value));
-        Assert.Equal("769283", value);
-
-        Assert.False(cfgProvider.TryGet("AppSettings:InactiveSetting", out _));
+        using var cfgProvider = BuildConfig<MySqlConnection, MySqlCommand>(
+            () => new MySqlConnection(MySqlServerConnectionString), db =>
+                new MySqlCommand(SqlQuery, db));
+        
+        AssertConfig(cfgProvider);
     }
 
     [Fact]
     public void DatabaseSettingsAreLoadable_Sqlite()
     {
         const string sqliteConnectionString = "Data Source=:memory:;";
+        var db = MakeSqlLiteDb(sqliteConnectionString);
+        using var cfgProvider = BuildConfig<SqliteConnection, SqliteCommand>(
+            () => db, db =>
+                new SqliteCommand(SqlQuery, db));
+        
+        AssertConfig(cfgProvider);
+    }
+
+    private static SqliteConnection MakeSqlLiteDb(string sqliteConnectionString)
+    {
         var db = new SqliteConnection(sqliteConnectionString);
 
         using (var queryCommand = new SqliteCommand(@"create table Settings
@@ -69,17 +64,13 @@ values
             db.Open();
 
             queryCommand.ExecuteNonQuery();
-
         }
 
-        using var cfgProvider =
-            new SqlServerConfigurationProvider<SqliteConnection>(new SqlServerConfigurationSource<SqliteConnection>()
-            {
-                DbConnection = () => db,
-                CreateQueryDelegate = db =>
-                    new SqliteCommand("SELECT SettingKey, SettingValue FROM Settings WHERE IsActive = 1", db)
-            });
+        return db;
+    }
 
+    private static void AssertConfig(ConfigurationProvider cfgProvider)
+    {
         cfgProvider.Load();
 
         Assert.True(cfgProvider.TryGet("AppSettings:DbIntSetting", out var value));
@@ -87,4 +78,12 @@ values
 
         Assert.False(cfgProvider.TryGet("AppSettings:InactiveSetting", out _));
     }
+
+    private static SqlServerConfigurationProvider<TCconneciton> BuildConfig<TCconneciton, TCommand>(
+        Func<TCconneciton> connection, Func<TCconneciton, DbCommand>? command) where TCconneciton : DbConnection =>
+        new(new SqlServerConfigurationSource<TCconneciton>()
+        {
+            DbConnection = connection,
+            CreateQueryDelegate = command
+        });
 }
